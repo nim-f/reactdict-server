ffb66c4b1cf4584c591d915d614885c0692d434f
Анна Шахбазян <fatamorgana9205@gmail.com>
2018-01-02 11:09:19 +0200

    google translate api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var translate = require('google-translate-api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let { word, from, to} = req.query;
+
+  translate(word, {from: from ? from : false, to: to ? to : 'en'}).then(response => {
+    console.log(response)
+    res.json(response.text)
+    //=> false
+  }).catch(err => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
